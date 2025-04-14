4b7c926e362e9b91c64414ff60dda0ecc86f131a
Marcos Kano <kanomarcos1974@gmail.com>
2025-04-14 07:44:21 -0700

    Tetst2

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+console.log("TST")
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
