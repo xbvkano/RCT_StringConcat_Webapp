4e705fa4e711bee1bc69a3e330694a2e276f3ead
Marcos Kano <kanomarcos1974@gmail.com>
2025-04-14 07:54:19 -0700

    last test

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-console.log("TST")
+console.log("TSeT")
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
